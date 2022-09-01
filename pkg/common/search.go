af1aa13b9c9e3d8c3107a98e26ee15d94fe853a2
dustinengle <dustin.engle@gmail.com>
2022-09-01 14:11:21 -0700

    move models to services

--- pkg/common/search.go
+++
@@ -1,7 +0,0 @@
-package common
-
-// Search is the request body for searching of
-// users for later use in a Lookup request.
-type Search struct {
-	Term string `json:"term"` // The search term for the query.
-}
--- pkg/test/result.go
+++
@@ -1,8 +0,0 @@
-package test
-
-// Result of a test run that contains the response code, data or error.
-type Result struct {
-	Code  int         // HTTP status code returned for the response.
-	Data  interface{} // Data if provided by the response.
-	Error error       // Error if provided.
-}
